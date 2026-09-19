1db762c598ae1a9f57b05bc186775e11de916e9a
Haspard Murphy <hmurphy@g.hmc.edu>
2026-09-19 14:55:30 -0700

    Lab3 Initialization

---
+++ labs/lab3/fpga/radiant/modules/lab3_hm.sv
@@ -0,0 +1,7 @@
+// -------------------------------------------------------------
+// lab2_hm.sv  
+// Author: Haspard Murphy <hmurphy@g.hmc.edu>
+// Date:   2026-09-12
+// Course: HMC E155, Lab 3
+// Purpose: Top-level module for Lab 3 instatiating...
+// -------------------------------------------------------------
